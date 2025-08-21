41593d7691062114238ec705af08a880e0b081ad
Mahmoud Sami <mahmoudsamisenna@gmail.com>
2025-08-21 12:10:11 +0300

    commit

--- src/test/java/Framework/TestComponents/BaseTest.java
+++ src/test/java/Framework/TestComponents/BaseTest.java
@@ -24,7 +24,7 @@ public class BaseTest {
 
     public WebDriver initializeDriver() {
         softAssert = new SoftAssert();
-        driver = new ChromeDriver();
+        driver = new EdgeDriver();
         driver.manage().deleteAllCookies();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
